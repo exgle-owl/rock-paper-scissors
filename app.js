7ab94920dcc3c20a5e6cf5d0f1ff008111fdfa3b
onizuka822 <pnikolaev822@gmail.com>
2022-12-09 18:03:34 +0300

    add toCapitalCase func

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const toCapitalCase = word => word.charAt(0).toUpperCase() + word.slice(1)
+
 const getComputerChoice = () => {
   const choices = ['rock', 'paper', 'scissors']
   return choices[Math.floor(Math.random() * 3)]
