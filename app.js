847c87fdeb68d0a7d7f86049425ec8d59661d947
onizuka822 <pnikolaev822@gmail.com>
2022-12-09 18:03:50 +0300

    add playRound func

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const getComputerChoice = () => {
   const choices = ['rock', 'paper', 'scissors']
   return choices[Math.floor(Math.random() * 3)]
 }
+
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase()
+
+  if (playerSelection === computerSelection) {
+    return 'Tie'
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'You won! Rock beat Scissors'
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'You won! Scissors beat Paper'
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'You won! Paper beat Rock'
+  } else {
+    return `You lose! ${toCapitalCase(computerSelection)} beat ${toCapitalCase(playerSelection)}`
+  }
+}
