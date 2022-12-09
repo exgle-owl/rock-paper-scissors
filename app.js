513b610f58c78a739c77307ac6ac851087a5a8a5
onizuka822 <pnikolaev822@gmail.com>
2022-12-09 18:17:58 +0300

    refactor playRound

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const playRound = (playerSelection, computerSelection) => {
 
   if (playerSelection === computerSelection) {
     return 'Tie'
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+  } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'rock')) {
     return 'You won! Rock beat Scissors'
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'You won! Scissors beat Paper'
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'You won! Paper beat Rock'
   } else {
     return `You lose! ${toCapitalCase(playerSelection)} beated by ${toCapitalCase(computerSelection)} `
   }
